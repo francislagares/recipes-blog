7a78d0f165f1019a40cbca3c6f64ff6edd0f63c7
Francis Lagares <francis.lagares@gmail.com>
2021-04-21 18:59:45 +0200

    style(layout): add normalize styles to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,9 @@ import { PageProps } from 'gatsby';
 import Navbar from '@/components/Footer';
 import Footer from '@/components/Navbar';
 
+import 'normalize.css';
+import '@/assets/main.css';
+
 const Layout: React.FC<PageProps> = ({ children }): JSX.Element => (
   <>
     <Navbar />

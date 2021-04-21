49fdb3396e8e0f1e962e792176cd9daa78a8e9ae
Francis Lagares <francis.lagares@gmail.com>
2021-04-21 19:01:18 +0200

    feat: add link pages to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,16 @@ const Navbar = (): JSX.Element => (
         <Link to="/">Home</Link>
       </li>
       <li>
+        <Link to="/recipes">Recipes</Link>
+      </li>
+      <li>
+        <Link to="/tags">Tags</Link>
+      </li>
+      <li>
         <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
       </li>
     </ul>
   </nav>

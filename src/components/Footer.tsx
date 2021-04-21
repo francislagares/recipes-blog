ba93cbc11c094d781adddcb121b8224e26d6bee0
Francis Lagares <francis.lagares@gmail.com>
2021-04-21 19:22:19 +0200

    style: add footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Footer = (): JSX.Element => <div>Footer</div>;
+const Footer = (): JSX.Element => (
+  <footer className="page-footer">
+    <p>
+      &copy; {new Date().getFullYear()} <span>SimplyRecipes</span> Built with
+      {` `}
+      <a href="https://www.gatsbyjs.com/">Gatsby</a>
+    </p>
+  </footer>
+);
 
 export default Footer;

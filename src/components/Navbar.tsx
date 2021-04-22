98563b4745aeb4dc231c812aeca10295fef619df
Francis Lagares <francis.lagares@gmail.com>
2021-04-22 14:33:15 +0200

    style: add navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,40 @@
+/* eslint-disable react/void-dom-elements-no-children */
 import React from 'react';
 import { Link } from 'gatsby';
+import { FaAlignJustify } from 'react-icons/fa';
+import logo from '@/assets/images/logo.svg';
 
 const Navbar = (): JSX.Element => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/recipes">Recipes</Link>
-      </li>
-      <li>
-        <Link to="/tags">Tags</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
+  <nav className="navbar">
+    <div className="nav-center">
+      <div className="nav-header">
+        <Link to="/">
+          <img src={logo} alt="Simply Recipes" />
+        </Link>
+        <button type="button" className="nav-btn">
+          <FaAlignJustify />
+        </button>
+      </div>
+      <div className="nav-links show-links">
+        <Link to="/" className="nav-link" activeClassName="active-link">
+          Home
+        </Link>
+        <Link to="/recipes" className="nav-link" activeClassName="active-link">
+          Recipes
+        </Link>
+        <Link to="/tags" className="nav-link" activeClassName="active-link">
+          Tags
+        </Link>
+        <Link to="/about" className="nav-link" activeClassName="active-link">
+          About
+        </Link>
+      </div>
+      <div className="nav-lilnks contact-link">
+        <Link to="/contact" className="btn">
+          Contact
+        </Link>
+      </div>
+    </div>
   </nav>
 );
 

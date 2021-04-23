4adf5d3f258d33beec95f9618d329b4a941e633f
Francis Lagares <francis.lagares@gmail.com>
2021-04-23 14:05:39 +0200

    feat: add header image to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '@/components/Layout';
 
 const Home = (): JSX.Element => (
   <main>
     <Layout>
-      <h1>Home Page</h1>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="Eggs"
+            placeholder="blurred"
+            layout="fullWidth"
+            className="hero-img"
+          />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Simply Recipes</h1>
+              <h4>no Fluff, Just Recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   </main>
 );

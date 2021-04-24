66c2c5d8837a94a277c87a394e941f6c737046ed
Francis Lagares <francis.lagares@gmail.com>
2021-04-24 18:59:39 +0200

    feat: create RecipesList component

---
+++ src/components/RecipesList.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { GatsbyImage, getImage, IGatsbyImageData } from 'gatsby-plugin-image';
+
+interface IRecipes {
+  id: string;
+  title: string;
+  cookTime: number;
+  prepTime: number;
+  image: IGatsbyImageData;
+  content: {
+    [key: string]: string[];
+  };
+}
+
+interface Props {
+  recipes: IRecipes[];
+}
+
+const RecipesList = ({ recipes = [] }: Props): JSX.Element => (
+  <div className="recipes-list">
+    {recipes.map((recipe) => {
+      const { id, title, image, prepTime, cookTime } = recipe;
+      const pathToImage = getImage(image);
+
+      return (
+        <Link to={`/${title}`} key={id} className="recipe">
+          <GatsbyImage image={pathToImage} className="recipe-img" alt={title} />
+          <h5>{title}</h5>
+          <p>
+            Prep: {prepTime}min | Cook: {cookTime}min
+          </p>
+        </Link>
+      );
+    })}
+  </div>
+);
+
+export default RecipesList;

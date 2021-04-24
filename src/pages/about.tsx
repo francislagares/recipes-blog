155a68c6e049a5644db4ae6f274fab06971ed707
Francis Lagares <francis.lagares@gmail.com>
2021-04-24 19:02:58 +0200

    feat: display featured recipes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,36 +1,66 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '@/components/Layout';
+import RecipesList from '@/components/RecipesList';
 
-const About = (): JSX.Element => (
-  <Layout>
-    <main className="page">
-      <section className="about-page">
-        <article>
-          <h2>I'm baby coloring book poke taxidermy</h2>
-          <p>
-            Taxidermy forage glossier letterpress heirloom before they sold out
-            you probably haven't heard of them banh mi biodiesel chia.
-          </p>
-          <p>
-            Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvi a
-            retro.
-          </p>
-          <Link to="/contact" className="btn">
-            contact
-          </Link>
-        </article>
-        <StaticImage
-          src="../assets/images/about.jpeg"
-          alt="Person Pouring Salt in Bowl"
-          className="about-img"
-          placeholder="blurred"
-        />
-      </section>
-    </main>
-  </Layout>
-);
+const About = ({ data }): JSX.Element => {
+  const recipes = data.allContentfulRecipe.nodes;
 
+  return (
+    <Layout>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm baby coloring book poke taxidermy</h2>
+            <p>
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably haven't heard of them banh mi biodiesel chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvi a
+              retro.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring Salt in Bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+        <section className="featured-recipes">
+          <h5>Look at this Awesomesouce!</h5>
+          <RecipesList recipes={recipes} />
+        </section>
+      </main>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`;
 export default About;
